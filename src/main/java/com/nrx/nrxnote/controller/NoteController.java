2cc5874443de908030c7a12991df69ca15e7be84
Nafiz <dhrubonill43@gmail.com>
2023-07-01 23:21:37 +0600

    feat: send data using ajax and prevent page reload

--- src/main/java/com/nrx/nrxnote/controller/NoteController.java
+++ src/main/java/com/nrx/nrxnote/controller/NoteController.java
@@ -1,12 +1,15 @@
 package com.nrx.nrxnote.controller;
 
+import co.elastic.clients.elasticsearch.nodes.Http;
 import com.nrx.nrxnote.dto.NoteRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class NoteController {
@@ -18,9 +21,10 @@ public class NoteController {
     }
 
     @PostMapping("/home")
-    public String save(@ModelAttribute NoteRequest noteRequest){
+    @ResponseBody
+    public HttpStatus save(@ModelAttribute NoteRequest noteRequest){
         System.out.println(noteRequest.getNoteHeader());
         System.out.println(noteRequest.getNoteMessage());
-        return "index";
+        return HttpStatus.OK;
     }
 }
